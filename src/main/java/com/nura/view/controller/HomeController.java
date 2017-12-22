92e8fb28dd270a0c0f8d87116d379ae449a95346
Arun Kumar <aprodigalboy@gmail.com>
2017-12-23 02:00:17 +0530

    Basic Security Spring configuration completed, to secure pages based on authorization

--- src/main/java/com/nura/view/controller/HomeController.java
+++ src/main/java/com/nura/view/controller/HomeController.java
@@ -12,4 +12,10 @@ public class HomeController {
 		System.out.println("Inside home page");
 		return "homePage";
 	}
+	
+	@RequestMapping(value = "/secured/profile", method = RequestMethod.GET)
+	public String profilePage() {
+		System.out.println("Inside profile page");
+		return "secure/profile";
+	}
 }
